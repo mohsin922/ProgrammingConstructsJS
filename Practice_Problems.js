a14dd703d53c64bcf1547cc36c262fb41cf87f71
peermohsin22 <peermohsin22@gmail.com>
2022-02-13 08:54:34 +0530

    Sequence Problems

---
+++ Practice_Problems.js
@@ -0,0 +1,62 @@
+// 1. Use REPL – Random Function 
+let Random_number = Math.floor(Math.random() * 10); 
+// let allows you to declare variables that are limited to the scope of a block statement
+console.log("Random Number is: " + Random_number); 
+
+
+// UC2:- Dice Number between 1 to 6 
+let Dice_Number = Math.floor(Math.random() * 6)+1;  //Dice Number between 1 to 6
+console.log("Dice Number is: " + Dice_Number ); 
+
+
+/* UC3:- Add two Random Dice Numbers*/
+let Dice_Number1 = Math.floor(Math.random()*6)+1;  //Dice Number between 1 to 6
+console.log(Dice_Number1 );  
+let Dice_Number2=Math.floor(Math.random()*6)+1;  //Dice Number between 1 to 6
+console.log(Dice_Number2 ); 
+console.log("Sum of Two Random Dice Number:- " + (Dice_Number1+Dice_Number2)); 
+
+
+/* UC4:- Use Script & Debug
+         – Write a program that reads 5 Random 2 Digit values, then find their sum and the average.
+*/
+
+let twoDigNum;
+let sum=0;
+for (let i = 0; i < 5; i++)
+{
+    twoDigNum = Math.floor(Math.random() * 90) + 10;
+    console.log('Number = ' + twoDigNum);
+    sum +=twoDigNum;   
+}  
+// finding sum and average 
+console.log('Sum of 5 two digits number is = ' + sum);
+console.log('Average of the sum of the 5 two digits number is = ' + sum/5);
+
+
+/* UC5:- Use Script & Debug – Unit Conversion .
+         a. 1ft = 12 in then 42 in = ? ft 
+         b. Rectangular Plot of 60 feet x 40 feet in meters
+         c. Calculate area of 25 such plots in acres
+*/
+//a. Conversion from inch to feet.
+let inchs = 42;
+let ft = inchs / 12;
+console.log("42 in = " + ft + " feet");
+
+//b. Rectangular Plot of 60 feet x 40 feet in meters.
+//convert feet to meter
+let len = 60;  
+let breadth = 40  
+//formula for rectangle area
+let area = (len*breadth);
+let areas = (area * 0.0929).toFixed(2);
+console.log('area in sq metres = ' + areas + ' sq-m');
+
+//calculate for 25 plots
+let totalArea = areas*25;
+console.log('Total Area of 25 such plots in sq-m = ' + totalArea + " sq-m" );
+
+//c. Calculating area of 25 such plots.
+let areaInAcre = (totalArea / 4047).toFixed(2);  // 1 acre = 4047 sq-m
+console.log('Total Area of 25 such plots in acre = ' + areaInAcre +' acres');
